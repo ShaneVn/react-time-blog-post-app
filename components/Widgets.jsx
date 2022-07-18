d4002d20d41ab006f48e27d35dd446eeef2855cc
Shane <shaneboo152@gmail.com>
2022-07-17 20:58:41 -0700

    development

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -1,7 +1,8 @@
-import { SearchIcon } from '@heroicons/react/outline'
-import React from 'react'
+import { SearchIcon } from "@heroicons/react/outline";
+import React from "react";
+import Trending from "./Trending"
 
-function Widgets() {
+function Widgets({ trendingResults, followResults }) {
   return (
     <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
       <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
@@ -14,8 +15,17 @@ function Widgets() {
           />
         </div>
       </div>
+
+      <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
+        <h4 className="font-bold text-xl px-4">what's happening</h4>
+        {trendingResults.map((result, index) => (
+          <Trending key={index} result={result} />
+        ))}
+        <button className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer
+        transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-medium">Show More</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Widgets
+export default Widgets;
